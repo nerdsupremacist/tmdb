994fc6bfd0e4f77790321b694992c7a14bf6ddcb
Mathias Quintero <me@quintero.io>
2020-04-01 11:34:53 +0200

    Adding fetching of movies from keyword

--- Sources/MovieDB/Common/Keyword.swift
+++ Sources/MovieDB/Common/Keyword.swift
@@ -1,10 +1,15 @@
 
 import Foundation
 import GraphZahl
+import NIO
 
 class Keyword: Decodable, GraphQLObject {
     let id: Int
     let name: String
+
+    func movies(client: Client) -> EventLoopFuture<Paging<Movie>> {
+        return client.get(at: "keyword", .constant(String(id)), "movies")
+    }
 }
 
 class Keywords: Decodable {
