a22b30126a6dad215e7758a56fbd1886487759a1
Mathias Quintero <me@quintero.io>
2020-09-27 14:12:43 +0200

    Fix first air date decoding issue

--- Sources/MovieDB/Media/TV/TVShow.swift
+++ Sources/MovieDB/Media/TV/TVShow.swift
@@ -12,7 +12,7 @@ class TVShow: Decodable, GraphQLObject {
     let backdrop: Image<BackdropSize>?
     let rating: Double
     let overview: String
-    let firstAirDate: Date
+    let firstAirDate: OptionalDate
     let originCountry: [String]
     let originalLanguage: String
     let numberOfRatings: Int
