62781c14329dfe80b2b95f4471881fde39794372
Mathias Quintero <me@quintero.io>
2021-05-17 20:17:36 +0200

    Fixing decoding of streaming links to work with deeplinks and standards

--- Sources/MovieDB/Media/Streaming/StreamingLinks.swift
+++ Sources/MovieDB/Media/Streaming/StreamingLinks.swift
@@ -2,9 +2,10 @@
 import Foundation
 import GraphZahl
 
-class StreamingLinks: Decodable, GraphQLObject {
+final class StreamingLinks: Decodable, GraphQLObject {
     enum CodingKeys: String, CodingKey {
-        case web = "deeplink_web"
+        case standardWeb = "standard_web"
+        case deeplinkWeb = "deeplink_web"
         case androidTV = "deeplink_android_tv"
         case tvOS = "deeplink_tvos"
         case fireTV = "deeplink_fire_tv"
@@ -15,10 +16,29 @@ class StreamingLinks: Decodable, GraphQLObject {
     let tvOS: URL?
     let fireTV: URL?
 
-    init(web: URL, androidTV: URL?, tvOS: URL?, fireTV: URL?) {
+    init(web: URL?, androidTV: URL?, tvOS: URL?, fireTV: URL?) {
         self.web = web
         self.androidTV = androidTV
         self.tvOS = tvOS
         self.fireTV = fireTV
     }
+
+    convenience init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let web: URL?
+        if let deeplink = try container.decodeIfPresent(URL.self, forKey: .deeplinkWeb) {
+            web = deeplink
+        } else if let standard = try container.decodeIfPresent(URL.self, forKey: .standardWeb) {
+            web = standard
+        } else {
+            web = nil
+        }
+
+        self.init(web: web,
+                  androidTV: try container.decodeIfPresent(URL.self, forKey: .androidTV),
+                  tvOS: try container.decodeIfPresent(URL.self, forKey: .tvOS),
+                  fireTV: try container.decodeIfPresent(URL.self, forKey: .fireTV))
+    }
 }
+
